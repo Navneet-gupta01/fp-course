b4f0f08f34ca13f6e8799683f954d3cc144b5179
Navneet <navneet.gupta.iitbhu@gmail.com>
2018-12-27 09:05:44 +0400

    changes

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -39,7 +39,7 @@ Problem --
 
 Consideration --
   Try to avoid repetition. Factor out any common expressions.
-  
+
 Example --
 Given file files.txt, containing:
   a.txt
@@ -61,7 +61,7 @@ To test this module, load ghci in the root of the project directory, and do
 Example output:
 
 $ ghci
-GHCi, version ... 
+GHCi, version ...
 Loading package...
 Loading ...
 [ 1 of 28] Compiling (etc...
@@ -85,46 +85,54 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile fileName contents = do
+  _ <- putStrLn ("============ " ++ fileName)
+  _ <- putStrLn contents
+  return ()
+  -- error "todo: Course.FileIO#printFile"
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles = void . sequence . (<$>) (uncurry printFile) -- Answer
+--list = sequence ((=<<) (\(x,y) -> (printFile x y)) list)
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile path = lift1 (\x -> (path, x)) (readFile path)
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles paths = sequence (lift1 getFile paths)
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@ and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run fileName = do
+  filePaths <- readFile fileName
+  toPrintContent <- getFiles (lines filePaths)
+  _ <- printFiles toPrintContent
+  return ()
+
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
 main =
-  error "todo: Course.FileIO#main"
+  getArgs >>= (\arguments ->
+      case arguments of
+        fileName:.Nil -> run fileName
+        _ -> putStrLn "Improper File Name")
 
 ----
 
